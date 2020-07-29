a91192ad726248f6f7937933e8f83f4d5dfe6888
dipti96 <diptippatel96@gmail.com>
2020-07-29 17:09:14 +0530

    dynamic dashboard page content , add assessment popup in dashboard page and change Breadcrumb of dashboard

--- src/domain/Dashboard/index.tsx
+++ src/domain/Dashboard/index.tsx
@@ -2,28 +2,121 @@ import * as React from 'react';
 import { Breadcrumbs } from '../../components/Breadcrumbs';
 import settingsIcon from '../../img/settings.png';
 import fileExportIcon from '../../img/file-export.png';
+import { AssessmentPopup } from './assetssmentPopup';
 
 export class Dashboard extends React.Component<any, any> {
     breadCrumbs: any;
+    assessmentRef: any;
     constructor(props: any) {
         super(props);
-        this.state = {};
+        this.state = {
+            dashboardData: [{
+                title: 'AWS Network Alerts',
+                awsdata: 'AWS (657907747545)',
+                percentage: '55%',
+                color: 'orange',
+            },
+            {
+                title: 'AWS HIPPA',
+                awsdata: 'AWS (657907747545)',
+                percentage: '92%',
+                color: 'yellow-green',
+            },
+            {
+                title: 'Azure CheckUp',
+                awsdata: 'AWS (657907747545)',
+                percentage: '9%',
+                color: 'red',
+            },
+            {
+                title: 'GCP CheckUp',
+                awsdata: 'AWS (657907747545)',
+                percentage: '',
+                color: '',
+            },
+            {
+                title: 'AWS Dashboard',
+                awsdata: 'AWS (657907747545)',
+                percentage: '66%',
+                color: 'orange',
+            },
+            {
+                title: 'AWS S3 Bucket',
+                awsdata: 'AWS (657907747545)',
+                percentage: '',
+                color: '',
+            },
+            {
+                title: 'AWS CIS Foundations',
+                awsdata: 'AWS (657907747545)',
+                percentage: '85%',
+                color: 'orange',
+            },
+            {
+                title: 'AWS NIST 800-53 Rev 4',
+                awsdata: 'AWS (657907747545)',
+                percentage: '2%',
+                color: 'red',
+            },
+            {
+                title: 'GCP CIS Foundations',
+                awsdata: 'AWS (657907747545)',
+                percentage: '35%',
+                color: 'orange',
+            },
+            {
+                title: 'Azure CIS Foundations',
+                awsdata: 'AWS (657907747545)',
+                percentage: '100%',
+                color: 'yellow-green',
+            },
+            ],
+        };
         this.breadCrumbs = [
             {
                 label: "Home",
                 route: `/`
             },
             {
-                label: "Monitor | Alerts",
+                label: "Compiiance | Dashboard",
                 isCurrentPage: true
             }
         ];
+        this.assessmentRef = React.createRef();
+    }
+
+    onClickonClickRunAssessment = (e: any) => {
+        this.assessmentRef.current.toggle();
+    };
+
+    displayDashboardData() {
+        const { dashboardData } = this.state;
+        const retData = [];
+        for (let i = 0; i < dashboardData.length; i++) {
+            const data = dashboardData[i];
+            retData.push(
+                <div className="col-xl-5 col-lg-4 col-md-6 col-sm-12">
+                    <div className="d-block compliance-main-box" onClick={this.onClickonClickRunAssessment}>
+                        <div className="d-block heading">
+                            <strong className="d-inline-block">{data.title}</strong>
+                            <i className="fa fa-caret-right float-right"></i>
+                        </div>
+                        <div className="d-block text-center compliance-inner">
+                            <p className="d-block">{data.awsdata}</p>
+                            {data.percentage != '' && <span className={data.color != '' ? data.color : 'd-block'}>{data.percentage}</span>}
+                            {data.percentage == '' && <a href="#" className="d-block">Click to run</a>}
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        return retData;
     }
     render() {
         const state = this.state;
         return (
             <div className="compliancemanager-dashboard-container">
-                <Breadcrumbs breadcrumbs={this.breadCrumbs} pageTitle="PREFORMANCE MANAGEMENT" />
+                <Breadcrumbs breadcrumbs={this.breadCrumbs} pageTitle="COMPLIANCE | DASHBOARD" />
                 <div className="compliancemanager-page-container">
                     <div className="common-container filter-container">
                         <div className="row">
@@ -75,17 +168,17 @@ export class Dashboard extends React.Component<any, any> {
                                         </div>
                                     </div>
                                     <div className="col-lg-3 col-md-4 col-sm-12">
-                                <div className="form-group filter-control-group clear-filters">
-                                    <label htmlFor="clearFilter">
-                                        <span>Clear All Filters</span>
-                                    </label>
-                                </div>
-                            </div>
+                                        <div className="form-group filter-control-group clear-filters">
+                                            <label htmlFor="clearFilter">
+                                                <span>Clear All Filters</span>
+                                            </label>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                             <div className="col-xl-4 col-lg-12 col-md-12 col-sm-12">
                                 <div className="float-right assessments">
-                                    <a href="#" className="blue-button">
+                                    <a onClick={this.onClickonClickRunAssessment} className="blue-button">
                                         <i className="fa fa-caret-right"></i>&nbsp;&nbsp; RUN ASSESSMENTS
                                     </a>
                                     <a href="#" className="white-button settings">
@@ -100,129 +193,11 @@ export class Dashboard extends React.Component<any, any> {
                     </div>
                     <div className="common-container">
                         <div className="row">
-                            <div className="col-xl-5 col-lg-4 col-md-6 col-sm-12">
-                                <div className="d-block compliance-main-box">
-                                    <div className="d-block heading">
-                                        <strong className="d-inline-block">AWS Network Alerts</strong>
-                                        <i className="fa fa-caret-right float-right"></i>
-                                    </div>
-                                    <div className="d-block text-center compliance-inner">
-                                        <p className="d-block">AWS (657907747545)</p>
-                                        <span className="d-block orange">55%</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-5 col-lg-4 col-md-6 col-sm-12">
-                                <div className="d-block compliance-main-box">
-                                    <div className="d-block heading">
-                                        <strong className="d-inline-block">AWS HIPPA</strong>
-                                        <i className="fa fa-caret-right float-right"></i>
-                                    </div>
-                                    <div className="d-block text-center compliance-inner">
-                                        <p className="d-block">AWS (657907747545)</p>
-                                        <span className="d-block yellow-green">92%</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-5 col-lg-4 col-md-6 col-sm-12">
-                                <div className="d-block compliance-main-box">
-                                    <div className="d-block heading">
-                                        <strong className="d-inline-block">Azure CheckUp</strong>
-                                        <i className="fa fa-caret-right float-right"></i>
-                                    </div>
-                                    <div className="d-block text-center compliance-inner">
-                                        <p className="d-block">AWS (657907747545)</p>
-                                        <span className="d-block red">9%</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-5 col-lg-4 col-md-6 col-sm-12">
-                                <div className="d-block compliance-main-box">
-                                    <div className="d-block heading">
-                                        <strong className="d-inline-block">GCP CheckUp</strong>
-                                        <i className="fa fa-caret-right float-right"></i>
-                                    </div>
-                                    <div className="d-block text-center compliance-inner">
-                                        <p className="d-block">AWS (657907747545)</p>
-                                        <a href="#" className="d-block">Click to run</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-5 col-lg-4 col-md-6 col-sm-12">
-                                <div className="d-block compliance-main-box">
-                                    <div className="d-block heading">
-                                        <strong className="d-inline-block">AWS Dashboard</strong>
-                                        <i className="fa fa-caret-right float-right"></i>
-                                    </div>
-                                    <div className="d-block text-center compliance-inner">
-                                        <p className="d-block">AWS (657907747545)</p>
-                                        <span className="d-block orange">66%</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-5 col-lg-4 col-md-6 col-sm-12">
-                                <div className="d-block compliance-main-box">
-                                    <div className="d-block heading">
-                                        <strong className="d-inline-block">AWS S3 Bucket</strong>
-                                        <i className="fa fa-caret-right float-right"></i>
-                                    </div>
-                                    <div className="d-block text-center compliance-inner">
-                                        <p className="d-block">AWS (657907747545)</p>
-                                        <a href="#" className="d-block">Click to run</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-5 col-lg-4 col-md-6 col-sm-12">
-                                <div className="d-block compliance-main-box">
-                                    <div className="d-block heading">
-                                        <strong className="d-inline-block">AWS CIS Foundations</strong>
-                                        <i className="fa fa-caret-right float-right"></i>
-                                    </div>
-                                    <div className="d-block text-center compliance-inner">
-                                        <p className="d-block">AWS (657907747545)</p>
-                                        <span className="d-block orange">85%</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-5 col-lg-4 col-md-6 col-sm-12">
-                                <div className="d-block compliance-main-box">
-                                    <div className="d-block heading">
-                                        <strong className="d-inline-block">AWS NIST 800-53 Rev 4</strong>
-                                        <i className="fa fa-caret-right float-right"></i>
-                                    </div>
-                                    <div className="d-block text-center compliance-inner">
-                                        <p className="d-block">AWS (657907747545)</p>
-                                        <span className="d-block red">2%</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-5 col-lg-4 col-md-6 col-sm-12">
-                                <div className="d-block compliance-main-box">
-                                    <div className="d-block heading">
-                                        <strong className="d-inline-block">GCP CIS Foundations</strong>
-                                        <i className="fa fa-caret-right float-right"></i>
-                                    </div>
-                                    <div className="d-block text-center compliance-inner">
-                                        <p className="d-block">AWS (657907747545)</p>
-                                        <span className="d-block orange">35%</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-xl-5 col-lg-4 col-md-6 col-sm-12">
-                                <div className="d-block compliance-main-box">
-                                    <div className="d-block heading">
-                                        <strong className="d-inline-block">Azure CIS Foundations</strong>
-                                        <i className="fa fa-caret-right float-right"></i>
-                                    </div>
-                                    <div className="d-block text-center compliance-inner">
-                                        <p className="d-block">AWS (657907747545)</p>
-                                        <span className="d-block yellow-green">100%</span>
-                                    </div>
-                                </div>
-                            </div>
+                            {this.displayDashboardData()}
                         </div>
                     </div>
                 </div>
+                <AssessmentPopup ref={this.assessmentRef} />
             </div>
         );
     }
