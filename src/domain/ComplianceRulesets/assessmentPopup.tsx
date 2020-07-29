cee4d965e9671e72714783db7610fe4f700be4d8
Satish pal <satishweb93@gmail.com>
2020-07-29 19:13:33 +0530

    Update: UI design for popup in compliance rulesets

--- src/domain/ComplianceRulesets/assessmentPopup.tsx
+++ src/domain/ComplianceRulesets/assessmentPopup.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 export class AssessmentPopup extends React.Component<any, any> {
     constructor(props: any) {
@@ -18,11 +18,55 @@ export class AssessmentPopup extends React.Component<any, any> {
     render() {
         const state = this.state;
         return (
-            <Modal isOpen={state.modal} toggle={this.toggle} className="">
+            <Modal isOpen={state.modal} toggle={this.toggle} className="modal-container">
                 <ModalHeader toggle={this.toggle}>Run Assessments</ModalHeader>
                 <ModalBody style={{ height: 'calc(75vh - 110px)', overflowY: 'auto', overflowX: "hidden" }}>
-                   
+                    <div className="d-block p-b-15 environment-text">Environment</div>
+                    <div className="form-group">
+                        <label htmlFor="rulesetGroup" className="d-block">
+                            Cloud account&nbsp;&nbsp;&nbsp;
+                            <i className="fa fa-info-circle"></i>
+                        </label>
+                        <select className="form-control d-block" id="rulesetGroup">
+                            <option>Select account</option>
+                            <option>Select account</option>
+                            <option>Select account</option>
+                            <option>Select account</option>
+                            <option>Select account</option>
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="rulesetGroup" className="d-block">
+                            Region&nbsp;&nbsp;&nbsp;
+                            <i className="fa fa-info-circle"></i>
+                        </label>
+                        <select className="form-control d-block" id="rulesetGroup">
+                            <option>Select Region</option>
+                            <option>Select Region</option>
+                            <option>Select Region</option>
+                            <option>Select Region</option>
+                            <option>Select Region</option>
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="rulesetGroup" className="d-block">
+                            VPC&nbsp;&nbsp;&nbsp;
+                            <i className="fa fa-info-circle"></i>
+                        </label>
+                        <select className="form-control d-block" id="rulesetGroup">
+                            <option>Select VPC</option>
+                            <option>Select VPC</option>
+                            <option>Select VPC</option>
+                            <option>Select VPC</option>
+                            <option>Select VPC</option>
+                        </select>
+                    </div>
                 </ModalBody>
+                <ModalFooter>
+                    <div className="d-block text-center">
+                        <button className="blue-button m-r-0 m-b-0">RUN</button>
+                    </div>
+                </ModalFooter>
             </Modal>
         );
     }
--- src/domain/ComplianceRulesets/newRuleSetPopup.tsx
+++ src/domain/ComplianceRulesets/newRuleSetPopup.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 export class NewRulSetPopup extends React.Component<any, any> {
     constructor(props: any) {
@@ -18,12 +18,38 @@ export class NewRulSetPopup extends React.Component<any, any> {
     render() {
         const state = this.state;
         return (
-            <Modal isOpen={state.modal} toggle={this.toggle} className="">
+            <Modal isOpen={state.modal} toggle={this.toggle} className="modal-container">
                 <ModalHeader toggle={this.toggle}>Create New Ruleset</ModalHeader>
                 <ModalBody style={{ height: 'calc(75vh - 110px)', overflowY: 'auto', overflowX: "hidden" }}>
-                   
+                    <div className="form-group">
+                        <label htmlFor="Name" className="d-block">Name</label>
+                        <input type="text" className="input-group-text d-block" placeholder="" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="Name" className="d-block">Description</label>
+                        <textarea className="input-group-text d-block"></textarea>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="rulesetGroup" className="d-block">
+                            Platform&nbsp;&nbsp;&nbsp;
+                            <i className="fa fa-info-circle"></i>
+                        </label>
+                        <select className="form-control d-block" id="rulesetGroup">
+                            <option>Select Platform</option>
+                            <option>Select Platform</option>
+                            <option>Select Platform</option>
+                            <option>Select Platform</option>
+                            <option>Select Platform</option>
+                        </select>
+                    </div>
                 </ModalBody>
+                <ModalFooter>
+                    <div className="d-block text-center">
+                        <button className="blue-button m-r-0 m-b-0">SAVE</button>
+                    </div>
+                </ModalFooter>
             </Modal>
+            
         );
     }
 }
