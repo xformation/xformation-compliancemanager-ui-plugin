c4f23e5492adf91fe8d4c93c40edc99bd5566a9b
Satish pal <satishweb93@gmail.com>
2020-07-30 19:57:11 +0530

    Update: Added page Compliance Policy and IU design continue.

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,3 +2,4 @@ export * from "./Dashboard";
 export * from "./ComplianceExclusions";
 export * from "./ComplianceRulesets";
 export * from "./ComplianceRemediation";
+export * from "./CompliancePolicy";
