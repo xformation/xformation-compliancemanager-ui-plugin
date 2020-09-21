81a14fe23e1bc8407301b531c2cb8dc3a7708e96
Satish pal <satishweb93@gmail.com>
2020-09-21 12:13:50 +0530

    Update: Table UI changes for all pages.

--- src/domain/ComplianceAssessmentHistory/index.tsx
+++ src/domain/ComplianceAssessmentHistory/index.tsx
@@ -96,61 +96,7 @@ export class ComplianceAssessmentHistory extends React.Component<any, any> {
         this.perPageLimit = 6,
         this.checkboxValue = false,
         this.state = {
-            searchKey: '',
-            historyData: [{
-                date: 'Jul 20, 2020 04:00 PM',
-                ruleName: 'AWS CCPA Framework',
-                environment: 'AWS (657907747545), Mumbai',
-                result: '64%',
-                failedTest: '97',
-                excludedTest: '0',
-                trigger: 'Manual'
-            },
-            {
-                date: 'Jul 20, 2020 05:00 PM',
-                ruleName: 'AWS CCPA Framework',
-                environment: 'AWS (657907747545), Mumbai',
-                result: '76%',
-                failedTest: '66',
-                excludedTest: '6',
-                trigger: 'Manual'
-            },
-            {
-                date: 'Jul 20, 2020 06:00 PM',
-                ruleName: 'AWS CCPA Framework',
-                environment: 'AWS (657907747545), Mumbai',
-                result: '34%',
-                failedTest: '44',
-                excludedTest: '3',
-                trigger: 'System'
-            }],
-            historyduplicatedata: [{
-                date: 'Jul 20, 2020 04:00 PM',
-                ruleName: 'AWS CCPA Framework',
-                environment: 'AWS (657907747545), Mumbai',
-                result: '64%',
-                failedTest: '97',
-                excludedTest: '0',
-                trigger: 'Manual'
-            },
-            {
-                date: 'Jul 20, 2020 05:00 PM',
-                ruleName: 'AWS CCPA Framework',
-                environment: 'AWS (657907747545), Mumbai',
-                result: '76%',
-                failedTest: '66',
-                excludedTest: '6',
-                trigger: 'Manual'
-            },
-            {
-                date: 'Jul 20, 2020 06:00 PM',
-                ruleName: 'AWS CCPA Framework',
-                environment: 'AWS (657907747545), Mumbai',
-                result: '34%',
-                failedTest: '44',
-                excludedTest: '3',
-                trigger: 'System'
-            }],
+            
         };
         this.breadCrumbs = [
             {
@@ -163,38 +109,6 @@ export class ComplianceAssessmentHistory extends React.Component<any, any> {
             }
         ];
 
-    }
-
-    createExclusionTable = () => {
-        const { historyData } = this.state;
-        const retData = [];
-        for (let i = 0; i < historyData.length; i++) {
-            const history = this.state.historyData[i];
-            retData.push(
-                <tr className="">
-                    <td className="">
-                        <div className="pointer-label">
-                            {/* <input type="checkbox" className="checkbox" /> */}
-                            {history.date}
-                        </div>
-                    </td>
-                    <td>{history.ruleName}</td>
-                    <td>{history.environment}</td>
-                    <td className="result-text">{history.result}</td>
-                    <td>{history.failedTest}</td>
-                    <td>{history.excludedTest}</td>
-                    <td>{history.trigger}</td>
-                    <td>
-                        <div className="d-flex">
-                            <Link to={`${config.basePath}/compliancehistory`} className="btn btn-link">
-                                <i className="fa fa-info-circle"></i>
-                            </Link>
-                        </div>
-                    </td>
-                </tr>
-            );
-        }
-        return retData;
     }
 
     render() {
@@ -274,33 +188,8 @@ export class ComplianceAssessmentHistory extends React.Component<any, any> {
                         </div>
                     </div>
                     <div className="common-container results-container">
-                        {/*  */}
-                        {/* <div className="exclusion-details">
-                            <div className="container-inner"> */}
                         <Table valueFromData={this.tableValue} perPageLimit={this.perPageLimit} visiblecheckboxStatus={this.checkboxValue}
                             tableClasses={{ table: "exclusion-data-table", tableParent: "container-inner", parentClass: "exclusion-details" }} searchKey="ruleName" showingLine="Showing %start% to %end% of %total% Assessment History" />
-
-                        {/* <table className="exclusion-data-table">
-                                    <tbody>
-                                        <tr className="exclusion-data-table-header">
-                                            <th>
-                                                <div className="pointer-label">
-                                                    Date
-                                            </div>
-                                            </th>
-                                            <th>Rule Name</th>
-                                            <th>Environment</th>
-                                            <th>Result</th>
-                                            <th># Failed Tests</th>
-                                            <th># Excluded Tests</th>
-                                            <th>Triggered By</th>
-                                            <th>Details</th>
-                                        </tr>
-                                        {this.createExclusionTable()}
-                                    </tbody>
-                                </table> */}
-                        {/* </div>
-                        </div> */}
                     </div>
 
                 </div>
