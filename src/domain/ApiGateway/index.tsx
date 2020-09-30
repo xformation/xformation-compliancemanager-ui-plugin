ce11b49a45613f39b05245ee4b0d301c7523a905
Satish pal <satishweb93@gmail.com>
2020-09-30 18:43:40 +0530

    Update: UI design for api.gateway page.

--- src/domain/ApiGateway/index.tsx
+++ src/domain/ApiGateway/index.tsx
@@ -61,7 +61,7 @@ export class ApiGateway extends React.Component<any, any> {
                             </ul>
                         </div>
                         <div className="row">
-                            <div className="col-lg-8 col-md-12 col-sm-12">
+                            <div className="col-xl-8 col-sm-12">
                                 <div className="gsl-editor-radio">
                                     <ul>
                                         <li>
@@ -88,23 +88,23 @@ export class ApiGateway extends React.Component<any, any> {
                                     </div>
                                     <div className="col-md-3 col-sm-3">
                                         <div className="form-group filter-control-group">
-                                            <label htmlFor="Account">Account</label>
-                                            <select className="form-control" id="Account">
-                                                <option value="">AWS</option>
-                                                <option value="">AWS</option>
-                                                <option value="">AWS</option>
-                                                <option value="">AWS</option>
+                                            <label htmlFor="Region">Region</label>
+                                            <select className="form-control" id="Region">
+                                                <option value="">All</option>
+                                                <option value="">All</option>
+                                                <option value="">All</option>
+                                                <option value="">All</option>
                                             </select>
                                         </div>
                                     </div>
                                     <div className="col-md-3 col-sm-3">
                                         <div className="form-group filter-control-group">
-                                            <label htmlFor="Account">Account</label>
-                                            <select className="form-control" id="Account">
-                                                <option value="">AWS</option>
-                                                <option value="">AWS</option>
-                                                <option value="">AWS</option>
-                                                <option value="">AWS</option>
+                                            <label htmlFor="VPC">VPC</label>
+                                            <select className="form-control" id="VPC">
+                                                <option value="">All</option>
+                                                <option value="">All</option>
+                                                <option value="">All</option>
+                                                <option value="">All</option>
                                             </select>
                                         </div>
                                     </div>
@@ -137,7 +137,36 @@ export class ApiGateway extends React.Component<any, any> {
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-lg-4 col-md-12 col-sm-12"></div>
+                            <div className="col-xl-4 col-sm-12">
+                                <div className="context-preview-box">
+                                    <h4>Context Preview</h4>
+                                    <div className="context-preview">
+                                        <div className="d-block form-group filter-search-control">
+                                            <form>
+                                                <input type="text" className="input-group-text" placeholder="Search" value="" />
+                                                <button><i className="fa fa-search"></i></button>
+                                            </form>
+                                        </div>
+                                        <div className="preview">
+                                            <ul>
+                                                <li>apiKeySource:<span>string</span></li>
+                                                <li>
+                                                    <i className="fa fa-caret-down"></i> binaryMediaTypes:Array <a href="#">[1]</a>
+                                                    <ul>
+                                                        <li>
+                                                            <i className="fa fa-caret-right"></i> O:Object
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                                <li>createdDate:<span>string</span></li>
+                                                <li>description:<span>string</span></li>
+                                                <li><i className="fa fa-caret-right"></i> endpointConfiguration:Object</li>
+                                                <li>minimumCompressionSize:<span>string</span></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
